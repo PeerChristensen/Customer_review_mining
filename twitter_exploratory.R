de00d814aafca406464cc98292077d3c425bc7e2
PeerChristensen <hr.pchristensen@gmail.com>
2018-12-09 11:12:37 +0100

    twitter plot

--- twitter_exploratory.R
+++ twitter_exploratory.R
@@ -19,5 +19,22 @@ df %<>%
 
 df[grep("Åge | Hareide",df$text),]
 
-df[grep("reklame",df$text),]
+reklame <- df %>%
+  filter(grepl("reklame",text)==T) %>% 
+  mutate(sentiment = map_int(text,happyorsad))
 
+df %>%
+  group_by(month=floor_date(date, "month")) %>%
+  count() %>%
+  filter(year(month) >= 2016) %>% 
+  ggplot(aes(x=month,y=n)) +
+  geom_col(fill="steelblue") +
+  theme_minimal() + 
+  theme(axis.text  = element_text(size = 16),
+        axis.title   = element_text(size = 18),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(margin = margin(r = 30,l=10)),
+        panel.grid = element_blank(),
+        strip.text.x = element_text(size=16))
+
+ggsave("tweets.png")
